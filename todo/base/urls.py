6052717af6efa34093c58bc5ecd1adbbf517cb99
Hamrish Saravanakumar <saravah@mcmaster.ca>
2021-05-08 22:40:20 -0400

    edits to implement login, logout, and user specific data features

--- todo/base/urls.py
+++ todo/base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
-
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='Login'),
+    path('logout/', LogoutView.as_view(next_page='Login'), name='Logout'),
     path('', TaskList.as_view(), name='Tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='Task'),
     path('create-task/', TaskCreate.as_view(), name='Create a Task'),
--- todo/base/views.py
+++ todo/base/views.py
@@ -3,28 +3,50 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Task
 
-class TaskList(ListView):
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__' #all fields should be shown
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('Tasks')
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'Tasks'
 
-class TaskDetail(DetailView):
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs) #to ensure that a user only has access to their own data
+        context['Tasks'] = context['Tasks'].filter(user=self.request.user)
+        context['count'] = context['Tasks'].filter(complete=False).count()
+        return context
+
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'Task'
     template_name = 'base/task.html'
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__' #all fields should be shown
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('Tasks')
 
-class TaskUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__' #all fields should be shown
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('Tasks')
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'Task'
     success_url = reverse_lazy('Tasks')
