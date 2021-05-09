7e852d42c8eaf81f280bf0e8918f9296a8762b61
Hamrish Saravanakumar <saravah@mcmaster.ca>
2021-05-08 21:05:25 -0400

    changes to account for Update and Delete views

--- todo/base/urls.py
+++ todo/base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     path('', TaskList.as_view(), name='Tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='Task'),
     path('create-task/', TaskCreate.as_view(), name='Create a Task'),
+    path('update-task/<int:pk>/', TaskUpdate.as_view(), name='Update Task'),
+    path('delete-task/<int:pk>/', TaskDelete.as_view(), name='Delete Task'),
 ]
--- todo/base/views.py
+++ todo/base/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
 class TaskList(ListView):
     model = Task
-    context_object_name = 'tasks'
+    context_object_name = 'Tasks'
 
 class TaskDetail(DetailView):
     model = Task
-    context_object_name = 'task'
+    context_object_name = 'Task'
     template_name = 'base/task.html'
 
 class TaskCreate(CreateView):
     model = Task
     fields = '__all__' #all fields should be shown
-    success_url = reverse_lazy('tasks')
+    success_url = reverse_lazy('Tasks')
 
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__' #all fields should be shown
+    success_url = reverse_lazy('Tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'Task'
+    success_url = reverse_lazy('Tasks')
